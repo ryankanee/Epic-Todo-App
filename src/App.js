aaeaad0560653db1ba487c2671e486fbf7b2078c
ryankanee <ryan.kane12@churchofjesuschrist.org>
2025-09-10 23:13:40 -0600

    INIT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import LogIn from "./components/loginPage";
+import TodoList from "./components/todoList";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoggin, setLogin] = useState(false);
+
+  const handleLogin = (username) => {
+    setCurrentUser(username);
+    setLogin(true);
+  };
+
+  const handleLogout = () => {
+    setLogin(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoggin ? (
+        <TodoList currentUser={currentUser} onLogout={handleLogout} />
+      ) : (
+        <LogIn onLogin={handleLogin} />
+      )}
     </div>
   );
 }
---
+++ src/components/todoList.js
@@ -0,0 +1,116 @@
+import { useEffect, useState } from "react";
+
+function TodoList({ currentUser, onLogout }) {
+  const [tasks, setTasks] = useState(() => {
+    const stored = localStorage.getItem(currentUser || "default");
+    return stored ? JSON.parse(stored) : [];
+  });
+  const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    const storageKey = currentUser || "default";
+    const stored = localStorage.getItem(storageKey);
+    setTasks(stored ? JSON.parse(stored) : []);
+  }, [currentUser]);
+
+  useEffect(() => {
+    const storageKey = currentUser || "default";
+    localStorage.setItem(storageKey, JSON.stringify(tasks));
+  }, [currentUser, tasks]);
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = (event) => {
+    event.preventDefault();
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedArray = tasks.filter((_, i) => i !== index);
+    setTasks(updatedArray);
+  };
+
+  const moveTaskUp = (index) => {
+    if (index > 0) {
+      const updatedArray = [...tasks];
+      [updatedArray[index], updatedArray[index - 1]] = [
+        updatedArray[index - 1],
+        updatedArray[index],
+      ];
+      setTasks(updatedArray);
+    }
+  };
+
+  const moveTaskDown = (index) => {
+    if (index < tasks.length - 1) {
+      const updatedArray = [...tasks];
+      [updatedArray[index], updatedArray[index + 1]] = [
+        updatedArray[index + 1],
+        updatedArray[index],
+      ];
+      setTasks(updatedArray);
+    }
+  };
+
+  const logout = () => {
+    onLogout();
+  };
+
+  return (
+    <>
+      <div className="user-group">
+        <p className="user">Welcome {currentUser}</p>
+        <button onClick={logout} className="logout-button">
+          Logout
+        </button>
+      </div>
+      <div className="todo-list">
+        <h1>Todo List!</h1>
+
+        <form onSubmit={addTask}>
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Add a task..."
+              value={newTask}
+              onChange={handleInputChange}
+            />
+            <button type="submit" className="add-button">
+              Add
+            </button>
+          </div>
+        </form>
+
+        <ol>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span className="text">{task}</span>
+              <button
+                className="delete-button"
+                onClick={() => deleteTask(index)}
+              >
+                Delete
+              </button>
+              <button className="move-button" onClick={() => moveTaskUp(index)}>
+                ⬆️
+              </button>
+              <button
+                className="move-button"
+                onClick={() => moveTaskDown(index)}
+              >
+                ⬇️
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </>
+  );
+}
+
+export default TodoList;
